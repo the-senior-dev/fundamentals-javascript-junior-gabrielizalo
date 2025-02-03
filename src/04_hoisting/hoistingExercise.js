38ba604616ca0234c3239b525e055d57388dcb21
Gabriel Porras <ghporras@gmail.com>
2025-02-03 18:20:26 -0500

    feat: Gabo Solution to 04 - Hoisting ⬆️

--- src/04_hoisting/hoistingExercise.js
+++ src/04_hoisting/hoistingExercise.js
@@ -1,8 +1,7 @@
 // Fix the code below to make the function return 12
 function calculate() {
-    a = 4;
-    let a;
-    b = 3;
+    const a = 4;
+    const b = 3;
     return a * b; // Should return 12
 }
 
